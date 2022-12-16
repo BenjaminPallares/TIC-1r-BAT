4d83102366dbf94326c7f77aec9dfa49b7371fa1
BenjaminPallares <120709730+BenjaminPallares@users.noreply.github.com>
2022-12-16 08:59:37 +0100

    Add files via upload

---
+++ UT2 - Activitat 1 - Varibles por pantalla/JS/scripts.js
@@ -0,0 +1,13 @@
+let a;
+let b;
+let c;
+
+a = 'Hola Mundo';
+b = 'Molo mogollón';
+c = 'Y lo sabes'
+
+// console.log(a);
+
+document.getElementById("result1").innerHTML = a;
+document.getElementById("result2").innerHTML = b;
+document.getElementById("result3").innerHTML = c;
